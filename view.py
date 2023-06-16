2b53d7465573d50b3a656a09def3f163c5d3bdf9
Konisaurus <yanikkonrad@bluewin.ch>
2023-06-16 14:53:56 +0200

    Store Calculation To Excel
    - After a calculation, the user can export the current calculation data
      to an existing excel file.
    - Create an new excel file through the GUI to store calculation data.
    
    Still to be done:
    - Exception handling for wrong paths.
    - Adding/Selecting sheets.

--- view.py
+++ view.py
@@ -4,9 +4,11 @@ This module contains the View class which contains all the visual aspects of the
 
 # Imports
 import tkinter as tk
+import json
 from widgets_for_gui import *
 from observer_subject import Observer
 from model_hypgeo import *
+from openpyxl import load_workbook, Workbook
 
 # Classes
 class View(tk.Tk, Observer):
@@ -69,7 +71,7 @@ class View(tk.Tk, Observer):
         self.lbl_deck_size.grid(row=0, column=0, padx=5, pady=5, sticky="nesw")
         self.lbl_sample_size.grid(row=0, column=2, padx=5, pady=5, sticky="nesw")
 
-        self.btn_calculate.grid(row=0, column=4, padx=20, pady=5, sticky="nesw")
+        self.btn_calculate.grid(row=0, column=4, padx=40, pady=5, sticky="nesw")
 
         self.ent_deck_size.grid(row=0, column=1, padx=5, pady=5, sticky="nesw")
         self.ent_sample_size.grid(row=0, column=3, padx=5, pady=5, sticky="nesw")
@@ -100,7 +102,7 @@ class View(tk.Tk, Observer):
         group = self.groups[key]
 
         group.append(tk.Frame(master=self.main_frm_bottom.get_frm_container()))   # Frame for one grouo
-        group.append(tk.Entry(master=group[0], font=("Helvetica", self.text_size), width=(2 * self.entry_width), validate="key"))
+        group.append(tk.Entry(master=group[0], font=("Helvetica", self.text_size), width=(2 * self.entry_width)))
         group.append(tk.Entry(master=group[0], font=("Helvetica", self.text_size), width=(self.entry_width), validate="key"))
         group.append(tk.Entry(master=group[0], font=("Helvetica", self.text_size), width=(self.entry_width), validate="key"))
         group.append(tk.Entry(master=group[0], font=("Helvetica", self.text_size), width=(self.entry_width), validate="key"))
@@ -156,7 +158,7 @@ class View(tk.Tk, Observer):
             pass
 
         elif update_event == "end calculte":
-            self.popup(("The probability of this configuration is: " + str(format_float(self.model.get_result(), factor=100)) + "%"))
+            self.popup_result()
         
         elif update_event == "invalid deck size":
             invalid_deck_size = int(self.ent_deck_size.get())
@@ -265,21 +267,125 @@ class View(tk.Tk, Observer):
         elif update_event == "key error":
             pass    
 
-    def popup(self, text):
+    def popup_result(self):
         '''
         Generates a popup in the middle of the application with "text".
         '''
-        popup = tk.Toplevel()                                        # Create a popup window.
-        popup.geometry("390x70")                                     # Set size.
-        popup.resizable(False, False)                                # Lock size.
+        self.popup_result = tk.Toplevel()                    # Create a popup window.
+        self.popup_result.geometry("390x70")                 # Set size.
+        self.popup_result.resizable(False, False)            # Lock size.
         x = self.winfo_x()
         y = self.winfo_y()
-        popup.geometry("+%d+%d" %(x+75,y+225))                      # Center the popup in front of the main window.
-        popup.grab_set()                                            # "Freezes" the main window until the popup is closed.
+        self.popup_result.geometry("+%d+%d" %(x+75,y+225))   # Center the popup in front of the main window.
+        self.popup_result.grab_set()                         # "Freezes" the main window until the popup is closed.
 
         # Label
-        lbl_popup = tk.Label(master=popup, text=text, height=1, font=("Helvetica", "11"), anchor="center")
-        lbl_popup.pack(padx=5, pady=20)
+        text = ("The probability of this configuration is: " + str(format_float(self.model.get_result(), factor=100)) + "%")
+        lbl_popup = tk.Label(master=self.popup_result, text=text, height=1, font=("Helvetica", "11"), anchor="center")
+        lbl_popup.pack(padx=5, pady=5)
 
-        popup.mainloop()
+        # Button
+        btn_export = tk.Button(master=self.popup_result, text="EXPORT TO EXCEL", font=("Helvetica", self.text_size), anchor="center", command=self.popup_export)
+        btn_export.pack(padx=5, pady=5)
+
+        self.popup_result.mainloop()
+
+    def popup_export(self):
+        '''
+        Popup for exporting the current data to an excel file.
+        '''
+        with open("refrences.json", "r") as read_storage:
+            path = json.load(read_storage)["excel path"]
+
+        self.popup_result.destroy()
+        self.popup_export = tk.Toplevel()                    # Create a popup window
+        self.popup_export.geometry("390x120")                # Set size.
+        self.popup_export.resizable(False, False)            # Lock size.
+        x = self.winfo_x()
+        y = self.winfo_y()
+        self.popup_export.geometry("+%d+%d" %(x+75,y+200))   # Center the popup in front of the main window.
+        self.popup_export.grab_set()                         # "Freezes" the main window until the popup is closed.
+
+        # Labels
+        lbl_path = tk.Label(master=self.popup_export, text="Path:", font=("Helvetica", self.text_size, "bold"), anchor="e")
+        lbl_path.grid(row=0, column=0, padx=5, pady=5)
+
+        # Entries
+        ent_path = tk.Entry(master=self.popup_export, font=("Helvetica", self.text_size), width=9*self.entry_width)
+        ent_path.insert("end", path)
+        ent_path.grid(row=0, column=1, padx=5, pady=5)
+
+        # Buttons
+        frm_btn = tk.Frame(master=self.popup_export, borderwidth=0, width=390)
+        frm_btn.grid(row=1, column=0, columnspan=2)
+        btn_export_to_path = tk.Button(master=frm_btn, text="EXPORT", font=("Helvetica", self.text_size), anchor="center", width=12, command=lambda: self.on_export(ent_path.get(), self.get_data()))
+        btn_create_at_path = tk.Button(master=frm_btn, text="CREATE NEW", font=("Helvetica", self.text_size), anchor="center", width=12, command=lambda: self.on_create_export(ent_path.get(), self.get_data()))
+        btn_export_to_path.grid(row=0, column=0, padx=30)
+        btn_create_at_path.grid(row=0, column=1, padx=30)
+
+    def get_data(self):
+        '''
+        Get entire relevant data for the current calculation and format it, so it can be entered into an excel file.
+        '''
+        data = [[], 
+                [], 
+                ["deck size", self.model.get_deck_size()],
+                ['sample size', self.model.get_sample_size()],
+                []]
         
+        model_defined_groups = self.model.get_defined_groups()
+        for key in model_defined_groups:
+            model_group = model_defined_groups[key]
+            group_data = [[self.groups[key][1].get()],
+                          ["cards in deck", model_group[0]],
+                          ["min. in sample", model_group[1]],
+                          ["max. in sample", model_group[2]],
+                          []
+                          ]
+            data.extend(group_data)
+
+        data.append(["probability", self.model.get_result()])
+
+        return data
+
+    def on_export(self, path, data):
+        '''
+        Load existing Excel file, add data.
+        '''
+        self.store_path(path)
+        wb = load_workbook(path)
+        self.export(path, data, wb)
+        
+
+    def on_create_export(self, path, data):
+        '''
+        Create new excel file, add data to it.
+        '''
+        self.store_path(path)
+        wb = Workbook()
+        self.export(path, data, wb)
+
+    def export(self, path, data, wb):
+        '''
+        Adds data to excel.
+        '''
+        ws = wb[wb.sheetnames[0]]
+
+        for row in data:
+            ws.append(row)
+        
+        wb.save(path)
+        wb.close()
+        self.popup_export.destroy()
+
+    def store_path(self, path):
+        '''
+        Stores last path in json.
+        '''
+        with open("refrences.json", "r") as read_storage:
+            refrences = json.load(read_storage)
+
+        refrences["excel path"] = path
+
+        with open("refrences.json", "w") as write_storage:
+            json.dump(refrences, write_storage, indent=4, separators=(",",":"))
